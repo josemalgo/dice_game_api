bd88e531b133995592efd7cf3ba273d9ada482b1
josemalgo <jose1432@hotmail.com>
2022-11-10 17:42:42 +0100

    callback listen

--- app/index.js
+++ app/index.js
@@ -5,11 +5,12 @@ import app from "./app.js";
 
 async function main() {
     const uri = mongoConfig.DB_URI;
-    
+
     try {
         mongoose.connect(uri);
-        app.listen(serverConfig.PORT);
-        console.log("Server is listening on port", 3000);
+        app.listen(serverConfig.PORT, () => {
+            console.log("Server is listening on port", serverConfig.PORT);
+        });
     } catch (error) {
         console.error("Unable to connect to the database: ", error);
     }
