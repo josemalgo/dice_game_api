903390b957ee08639aa5f7d1d96ed3fbcdb55f63
josemalgo <jose1432@hotmail.com>
2023-01-31 14:20:38 +0100

    add login.service and jwt

--- app/controllers/login.controller.js
+++ app/controllers/login.controller.js
@@ -1,6 +1,9 @@
 import { validateRequest } from "../helpers/helpers"
+import * as loginService from "../services/login.service.js"
 
-export const validateUser = (req, res, next) => {
+export const validateUser = async (req, res, next) => {
     validateRequest(req)
-
+    const { name, password } = req.body
+    const token = await loginService.loginPlayer(name, password)
+    res.status(200).json(token)
 }
--- app/services/login.service.js
+++ app/services/login.service.js
@@ -2,23 +2,22 @@ import { duplicatePlayerName } from "../helpers/helpers"
 import { Player } from "../models/Player"
 import bcrypt from "bcrypt"
 import Api401Error from "../middlewares/errors/api401Error"
+import jwt from "jsonwebtoken"
 
 export const loginPlayer = async (name, password) => {
-    const player = Player.findOne({name})
-
+    const player = await Player.findOne({name})
     const correctPassword = player === null 
         ? false
         : await bcrypt.compare(password, player.passwordhash)
     
-    if(!correctPassword) {
-        throw new Api401Error("Invalid user or password")
+    if(!(player && correctPassword)) {
+        throw new Api401Error("Invalid user or password.")
     }
 
     const userForToken = {
-        id: user._id,
-        password: user.password
+        name: player.name
     }
 
-    
+    const token = jwt.sign(userForToken, process.env.SECRETKEY)      
 
 }
